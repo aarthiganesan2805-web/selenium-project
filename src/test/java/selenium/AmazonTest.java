c38cd5f3a416e002d05b844eef537edd0ac30e95
Aarthi <aarthiganesan2805@gmail.com>
2026-08-19 22:30:16 +0530

    test added

---
+++ src/test/java/selenium/AmazonTest.java
@@ -0,0 +1,11 @@
+package selenium;
+
+import org.testng.annotations.Test;
+
+public class AmazonTest {
+
+    @Test
+    public void amazonTest() {
+        System.out.println("Amazon Selenium test running");
+    }
+}
